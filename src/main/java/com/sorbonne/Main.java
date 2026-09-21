baa2135ef793ee7121a3588df57c759171c43943
IDRIS ACHABOU <achabou02idris@gmail.com>
2026-09-21 12:37:07 +0200

    feat(core) add Apache Ant config

--- src/main/java/com/sorbonne/Main.java
+++ src/main/java/com/sorbonne/Main.java
@@ -4,10 +4,8 @@ package com.sorbonne;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
