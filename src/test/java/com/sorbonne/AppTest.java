3ed097a84ba93f2f3d69e7d370f8de5c9d7b5ce0
IDRIS ACHABOU <achabou02idris@gmail.com>
2026-09-21 11:55:07 +0200

    feat(arch) : Project architecture init

---
+++ src/test/java/com/sorbonne/AppTest.java
@@ -0,0 +1,20 @@
+package com.sorbonne;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
